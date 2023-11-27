fe3388cc7a5a9e37d28fcd6a93d9db8c591a374e
Doudousml <smailourdia31@gmail.com>
2023-11-27 11:37:04 +0100

    ajout et modification

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const cacheName = "ginkobus-pwa-v1";
+const contentToCache =[
+
+
+"icons/icon-192.png",
+"icons/icon-512.png",
+"icons/maskable_icon.png",
+"ginko.webmanifest",
+"app.js",
+"index.html",
+"style.css"]
+self.addEventListener("install", (e) => {
+    console.log("[Service Worker] Install");
+    e.waitUntil(
+      (async () => {
+        const cache = await caches.open(cacheName);
+        console.log("[Service Worker] Caching all: app shell and content");
+        await cache.addAll(contentToCache);
+      })(),
+    );
+  });
